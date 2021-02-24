235cf6fb56cd35f34a9279ce370c43295814dee4
flogea <katua.logvinova@gmail.com>
2021-02-24 21:28:29 +0300

    weather block

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-document.getElementById('btn_search') = btn_search;
+window.onload = function () {
+    document.getElementById('btn_search').onclick = btn_search;
+
+    function btn_search () {
+       document.querySelector('.wrapper').style.display = 'block';
+       
+    }
+}
