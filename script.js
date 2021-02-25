e94a88d4ecf6f74fd3abcd6160b135fb38948ca0
flogea <katua.logvinova@gmail.com>
2021-02-25 21:54:13 +0300

    add little boxes

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
 window.onload = function () {
+    const keyAPI = '09d9181db657691a7f0679fb629d686f';
+
     document.getElementById('btn_search').onclick = btn_search;
 
     function btn_search () {
-       document.querySelector('.wrapper').style.display = 'block';
-       
+       document.querySelector('.wrapper').style.display = 'block';  
     }
 
-    fetch('http://api.openweathermap.org/data/2.5/weather?id=498817&appid=09d9181db657691a7f0679fb629d686f')
+    fetch(`http://api.openweathermap.org/data/2.5/weather?id=498817&appid=${keyAPI}`)
     .then(function (resp) {return resp.json() })
     .then(function (data) {
         console.log(data);
         document.querySelector('.city_name').textContent = data.name;
-        document.querySelector('.today_degrees').innerHTML = Math.round(data.main.temp - 273) + '&deg';
+        document.querySelector('#today_real_temp').innerHTML += Math.round(data.main.temp - 273) + '&deg';
+        document.querySelector('#today_feelslike_temp').innerHTML += Math.round(data.main.feels_like - 273) + '&deg';
         document.querySelector('.today_weather_conditions').textContent = data.weather[0]['description'];
     })
+
+    fetch(`http://api.openweathermap.org/data/2.5/forecast?id=498817&appid=${keyAPI}`)
+    .then(function (resp) {return resp.json() })
+    .then(function (data) {
+        console.log(data);
+    })
+
+    function location (location) {
+
+    }
 }
