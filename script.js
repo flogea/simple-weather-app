9fd3d488f82c39b57e7edacef511c65405212f13
flogea <katua.logvinova@gmail.com>
2021-03-03 18:20:51 +0300

    gifs depends on the weather cond.

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 window.onload = function () {
     const keyAPI = '09d9181db657691a7f0679fb629d686f';
-
+    var bg_gif = document.querySelector('.today_weather');
     document.querySelector('.submit').onclick = btn_search; // Разобраться с классом и id
+    const cloudweath = 'img/cloud/cloud.gif';
+    const rainweath = 'img/rain/puddle.gif';
+    const clearweath = 'img/clear/clearSky.gif';
 
     function btn_search () {
         const cityName_inp = document.querySelector('.input_search').value; // Разобраться, можно ли это решить с классом, а не id
@@ -18,6 +21,17 @@ window.onload = function () {
                     document.querySelector('#today_real_temp').innerHTML += Math.round(data.main.temp - 273) + '&deg';
                     document.querySelector('#today_feelslike_temp').innerHTML += Math.round(data.main.feels_like - 273) + '&deg';
                     document.querySelector('.today_weather_conditions').textContent = data.weather[0]['description'];
+                    switch (data.weather[0]['main']) {
+                        case 'Clouds':
+                            bg_gif.style.background = `rgba(0, 0, 0, 0.4) url(${cloudweath})`;
+                            break;
+                        case 'Clear':
+                            bg_gif.style.background = `rgba(0, 0, 0, 0.4) url(${clearweath})`;
+                            break;
+                        case 'Rain':
+                            bg_gif.style.background = `rgba(0, 0, 0, 0.4) url(${rainweath})`;
+                            break;
+                    }
                 })
     
             fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${cityName_inp}&appid=${keyAPI}`)
