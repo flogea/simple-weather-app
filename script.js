de59f2fce60982db926d8dc85502a94fd54845ac
flogea <katua.logvinova@gmail.com>
2021-03-03 14:36:29 +0300

    search city from input

--- script.js
+++ script.js
@@ -4,26 +4,27 @@ window.onload = function () {
     document.getElementById('btn_search').onclick = btn_search;
 
     function btn_search () {
-       document.querySelector('.wrapper').style.display = 'block';  
-    }
+       const cityName_inp = document.querySelector('#city').value;
+       document.querySelector('.wrapper').style.display = 'block'; 
 
-    fetch(`http://api.openweathermap.org/data/2.5/weather?id=498817&appid=${keyAPI}`)
-    .then(function (resp) {return resp.json() })
-    .then(function (data) {
-        console.log(data);
-        document.querySelector('.city_name').textContent = data.name;
-        document.querySelector('#today_real_temp').innerHTML += Math.round(data.main.temp - 273) + '&deg';
-        document.querySelector('#today_feelslike_temp').innerHTML += Math.round(data.main.feels_like - 273) + '&deg';
-        document.querySelector('.today_weather_conditions').textContent = data.weather[0]['description'];
-    })
+       fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName_inp}&appid=${keyAPI}`)
+        .then(function (resp) {return resp.json() })
+        .then(function (data) {
+            console.log(data);
+            document.querySelector('.city_name').textContent = data.name;
+            document.querySelector('#today_real_temp').innerHTML += Math.round(data.main.temp - 273) + '&deg';
+            document.querySelector('#today_feelslike_temp').innerHTML += Math.round(data.main.feels_like - 273) + '&deg';
+            document.querySelector('.today_weather_conditions').textContent = data.weather[0]['description'];
+        })
 
-    fetch(`http://api.openweathermap.org/data/2.5/forecast?id=498817&appid=${keyAPI}`)
-    .then(function (resp) {return resp.json() })
-    .then(function (data) {
-        console.log(data);
-    })
+        fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${cityName_inp}&appid=${keyAPI}`)
+        .then(function (resp) {return resp.json() })
+        .then(function (data) {
+            console.log(data);
+        })
 
-    function location (location) {
+        function location (location) {
 
+        } 
     }
 }
